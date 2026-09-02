8b3653da3420159e5b3b0cfdc990f533d406c6e2
Merry <svgr1808@outlook.de>
2026-09-02 15:30:10 +0200

    build: require java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,19 +17,19 @@ subprojects {
 
     tasks {
         java {
-            toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-            sourceCompatibility = JavaVersion.VERSION_21
-            targetCompatibility = JavaVersion.VERSION_21
+            toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+            sourceCompatibility = JavaVersion.VERSION_25
+            targetCompatibility = JavaVersion.VERSION_25
         }
 
         compileJava {
             options.encoding = "UTF-8"
-            options.release.set(21)
+            options.release.set(25)
         }
 
         compileTestJava {
             options.encoding = "UTF-8"
-            options.release.set(21)
+            options.release.set(25)
         }
     }
 }

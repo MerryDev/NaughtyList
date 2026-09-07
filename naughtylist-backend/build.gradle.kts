610213af5cf25c99a777419745701199e0f34888
Merry <svgr1808@outlook.de>
2026-09-07 18:18:23 +0200

    build: migrate dependencies to gradle version catalogue

--- naughtylist-backend/build.gradle.kts
+++ naughtylist-backend/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.4.10"
-    kotlin("plugin.spring") version "2.4.10"
-    id("org.springframework.boot") version "4.2.0-SNAPSHOT"
-    id("io.spring.dependency-management") version "1.1.7"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependencyManagement)
 }
 
 repositories {
